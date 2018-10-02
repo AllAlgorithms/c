f939385a625417e00beb915575c6c3a11b17a5a4
Sladyn <gunnerforlife00@gmail.com>
2018-10-02 11:54:49 +0530

    new palindrome alogrithm

---
+++ Strings/palindrome.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+ 
+int main()
+{
+  char text[100];
+  int begin, middle, end, length = 0;
+ 
+  gets(text);
+ 
+  while (text[length] != '\0')
+    length++;
+ 
+  end = length - 1;
+  middle = length/2;
+ 
+  for (begin = 0; begin < middle; begin++)
+  {
+    if (text[begin] != text[end])
+    {
+      printf("Not a palindrome.\n");
+      break;
+    }
+    end--;
+  }
+  if (begin == middle)
+    printf("Palindrome.\n");
+ 
+  return 0;
+}
