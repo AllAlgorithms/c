6c3b87520f225185d60f3eeff22d8f11ca1bd2ef
AbderreZzaQ <sendjasni.abderrezzaq@gmail.com>
2019-02-24 11:52:14 +0100

    Update matmul.c
    Added initialization and display function

--- math/Matrix Multiplication/matmul.c
+++ math/Matrix Multiplication/matmul.c
@@ -9,73 +9,85 @@
 #include <stdlib.h>
 #include <time.h>
 
-const int MAT_SIZE = 500;
+#define MAT_SIZE 50
 
-void MatrixMultiplication()
+double first_mat[MAT_SIZE][MAT_SIZE];
+double second_mat[MAT_SIZE][MAT_SIZE];
+double multi_result[MAT_SIZE][MAT_SIZE];
+
+void MatrixInitialisation()
 {
-	double first_mat[MAT_SIZE][MAT_SIZE];
-	double second_mat[MAT_SIZE][MAT_SIZE];
-	srand(time(NULL));
+    srand(time(NULL));
 
-	/*
+    /*
 		Initialise Matrix randomly using the rand() function
 		*/
 
-	for (int i = 0; i < MAT_SIZE; i++)
-	{
-		for (int j = 0; j < MAT_SIZE; j++)
-		{
-			first_mat[i][j] = rand() % 100;
-			second_mat[i][j] = rand() % 100;
-		}
-	}
+    for (int i = 0; i < MAT_SIZE; i++)
+    {
+        for (int j = 0; j < MAT_SIZE; j++)
+        {
+            first_mat[i][j] = rand() % 100;
+            second_mat[i][j] = rand() % 100;
+        }
+    }
+}
+void MatrixMultiplication()
+{
 
-	int sum;
-	double multi_result[MAT_SIZE][MAT_SIZE];
+    int sum;
 
-	struct timespec start_time;
-	int begin = clock();
-	clock_gettime(CLOCK_REALTIME, &start_time); /* Get cpu time before the multiplication*/
+    for (int i = 0; i < MAT_SIZE; i++)
+    {
+        for (int j = 0; j < MAT_SIZE; j++)
+        {
+            sum = 0;
+            for (int k = 0; k < MAT_SIZE; k++)
+            {
+                sum = sum + first_mat[i][k] * second_mat[k][j];
+            }
+            multi_result[i][j] = sum;
+        }
+    }
+}
 
-	for (int i = 0; i < MAT_SIZE; i++)
-	{
-		for (int j = 0; j < MAT_SIZE; j++)
-		{
-			sum = 0;
-			for (int k = 0; k < MAT_SIZE; k++)
-			{
-				sum = sum + first_mat[i][k] * second_mat[k][j];
-			}
-			multi_result[i][j] = sum;
-		}
-	}
-
-	struct timespec finish_time;
-	int end = clock();
-	clock_gettime(CLOCK_REALTIME, &finish_time); /* Get cpu time after the multiplication*/
-
-	/*
+void DisplayMatrix()
+{
+    /*
 		Display the multiplication result
 		*/
-	fprintf(stderr, "****************** Result ****************** \n");
-	for (int i = 0; i < MAT_SIZE; i++)
-	{
-		for (int j = 0; j < MAT_SIZE; j++)
-		{
-			fprintf(stderr, "multi_result[%d][%d] = %f\n", i, j, multi_result[i][j]);
-		}
-	}
-
-	fprintf(stderr, "\n *************** Mat Multiplication Stat *************** \n");
-	fprintf(stderr, "Execution time : %f sec. \n", (float)((finish_time.tv_sec) - (start_time.tv_sec)));
-	fprintf(stderr, "Execution time : %f nsec. \n", (float)((finish_time.tv_nsec) - (start_time.tv_nsec)));
-	fprintf(stderr, "The execution clock() : %f\n ", (float)(end - begin) / (float)(CLOCKS_PER_SEC));
+    fprintf(stderr, "****************** Result ****************** \n");
+    for (int i = 0; i < MAT_SIZE; i++)
+    {
+        for (int j = 0; j < MAT_SIZE; j++)
+        {
+            fprintf(stderr, "%f\t", multi_result[i][j]);
+        }
+        fprintf(stderr, "\n");
+    }
 }
 
 int main(int argc, char const *argv[])
 {
 
-	MatrixMultiplication(MAT_SIZE);
+    MatrixInitialisation();
 
-	return EXIT_SUCCESS;
+    struct timespec start_time;
+    int begin = clock();
+    clock_gettime(CLOCK_REALTIME, &start_time); /* Get cpu time before the multiplication*/
+
+    MatrixMultiplication();
+
+    struct timespec finish_time;
+    int end = clock();
+    clock_gettime(CLOCK_REALTIME, &finish_time); /* Get cpu time after the multiplication*/
+
+    DisplayMatrix();
+
+    fprintf(stderr, "\n *************** Mat Multiplication Stat *************** \n");
+    fprintf(stderr, "Execution time : %f sec. \n", (float)((finish_time.tv_sec) - (start_time.tv_sec)));
+    fprintf(stderr, "Execution time : %f nsec. \n", (float)((finish_time.tv_nsec) - (start_time.tv_nsec)));
+    fprintf(stderr, "The execution clock() : %f\n ", (float)(end - begin) / (float)(CLOCKS_PER_SEC));
+
+    return EXIT_SUCCESS;
 }
