33bea7a9bac8de44a01fa355e3cde34a43513200
Natanel Ziv <natanel.ziv@motorolasolutions.com>
2019-07-25 09:38:15 +0300

    Adding simple TCP server client files

---
+++ networking/basic _client_server_tcp/client/TCP_receiver.h
@@ -0,0 +1,17 @@
+//
+// Created by NWT743 on 25/07/2019.
+//
+
+#ifndef C_1_TCP_RECEIVER_H
+#define C_1_TCP_RECEIVER_H
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#endif //C_1_TCP_RECEIVER_H
