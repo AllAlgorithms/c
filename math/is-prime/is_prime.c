873fe652dc7c3dff292aecf1267f127ff1595417
Harsh2803 <49323194+Harsh2803@users.noreply.github.com>
2019-10-02 13:18:38 +0530

    updated technique

--- math/is-prime/is_prime.c
+++ math/is-prime/is_prime.c
@@ -1,43 +1,31 @@
 #include <stdio.h>
 
 // Returns 1 if the given number is prime, 0 otherwise 
-int is_prime(unsigned long x) {
-    switch (x) {
-        case 0:
-        case 1:
-            return 0;
-
-        case 2:
-            return 1;
-
-        default: {
-            unsigned int i = 0;
-            // Multiples of 2 are not prime
-            if (0 == (x % 2)) {
-                return 0;
-            }
-
-            // Test odd numbers from 3 up to the square root of x
-            for (i = 3; (i * i) <= x; i += 2) {
-                if(0 == (x % i)) {
-                    return 0;
-                }
-            }
-
-            return 1;
-        }
+int is_prime(int x) {
+   if(x%2==0){
+       return 1;
+   }
+    else{
+        return 0;
     }
 }
 
+
 int main()
 {
-    unsigned long x = 0;
-
+    int x = 0;
+    int i=0;
     while (1) {
         printf("Provide an integer: \n");
-        scanf("%lu", &x);
+        scanf("%d", &x);
 
-        printf("%lu is%s prime\n", x, is_prime(x) ? "" : " not");
+       i=is_prime(x);
+        if(i==1){
+            printf("prime");
+        }
+        else{
+            printf("not prime");
+        }
 
     }
 
