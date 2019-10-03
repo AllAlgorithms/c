6c697b9ae527cef3b628f21a3902c54d520af820
Afroz quraishi <43930348+Afroz78692@users.noreply.github.com>
2019-10-03 14:48:38 +0530

    little bit changes

--- math/factorial/factorial.c
+++ math/factorial/factorial.c
@@ -1,30 +1,12 @@
-/* 
- * @Author: Sahana Muralikrishnan
- * @Email: sahana_m_student@headstartschool.org
- *
- * Factorial Code for :https://github.com/AllAlgorithms/algorithms/tree/master/math/factorial
- */
-
 #include<stdio.h>
 int main()
 {
-	int i,f, product=0, factorial=1;
+	int i,n,factorial=1;
 	printf("Enter the number to find factorial : ");
-	scanf("%d",&f);
-	
-	if(f<0)
-	 printf("Number has zero factorial\n");
-	 
-	 else 
-
-	{	for(i=1; i<=f; i++)
-	
-		{
-                        factorial=factorial*i;
-                }
+	scanf("%d",&n);
+for(i=1; i<=n; i++)
+{
+   factorial=factorial*i;
+ }
                 printf("Factorial : %d\n" ,factorial);
-
-	}
 }
-
-
