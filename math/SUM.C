6c5c9e4445b6ecfb1e309f875e5ffec8f6dd2074
Shuvam <shuvamkumar2001@gmail.com>
2019-10-17 23:15:10 +0530

    Create SUM.C

---
+++ math/SUM.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+printf("Enter 2 numbers");
+scanf("%d %d",&a,&b);
+printf("Sum of the given numbers is : %d",a+b);
+getch();
+return 0;
+}
