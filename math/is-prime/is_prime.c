0596991c3021565b2130d729da04b31ec799de33
maneu123 <51678755+maneu123@users.noreply.github.com>
2019-10-21 23:26:29 -0300

    is_prime.c
    the "is_prime" function has been decreased and made more efficient

--- math/is-prime/is_prime.c
+++ math/is-prime/is_prime.c
@@ -2,31 +2,17 @@
 
 // Returns 1 if the given number is prime, 0 otherwise 
 int is_prime(unsigned long x) {
-    switch (x) {
-        case 0:
-        case 1:
-            return 0;
-
-        case 2:
-            return 1;
-
-        default: {
-            unsigned int i = 0;
-            // Multiples of 2 are not prime
-            if (0 == (x % 2)) {
-                return 0;
-            }
-
-            // Test odd numbers from 3 up to the square root of x
-            for (i = 3; (i * i) <= x; i += 2) {
-                if(0 == (x % i)) {
-                    return 0;
-                }
-            }
-
-            return 1;
+    int i;
+    if(x==0 || x==1)return 0;
+    else{
+        //"div" counts the number of divisors of x
+        char div=49;
+        for(i=2; i<=x; i++){
+            if(x%i==0) div++;
+            if(div>50) return 0;
         }
     }
+    return 1;
 }
 
 int main()
@@ -36,9 +22,7 @@ int main()
     while (1) {
         printf("Provide an integer: \n");
         scanf("%lu", &x);
-
-        printf("%lu is%s prime\n", x, is_prime(x) ? "" : " not");
-
+        printf("%lu is%s prime\n\n", x, is_prime(x)?"":" not");
     }
 
     return 0;
