6380ba6abb53e1582042dbeffaa4bb67f54d4739
Saksham Doda <51626236+sakshamdoda@users.noreply.github.com>
2019-10-22 17:30:15 +0530

    Create pattern.c

---
+++ pattern.c
@@ -0,0 +1,30 @@
+ABCD Pattern
+//ABCDEEDCBA
+//ABCDDCBA
+//ABCCBA
+//ABBA
+//AA
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        char ch='A';
+        for(int j=1;j<=n-i;j++)
+        {
+            printf("%c",ch);
+            ch=ch+1;
+        }
+        ch=ch-1;
+        for(int j=1;j<=n-i;j++)
+        {
+            printf("%c",ch);
+            ch=ch-1;
+        }
+        printf("\n");
+    }
+    return 0;
+}
