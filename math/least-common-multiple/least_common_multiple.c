eb7ff3ee3b34592e6d9d17c6fcd10773e47903c3
GauravSingh9356 <55052983+GauravSingh9356@users.noreply.github.com>
2019-10-26 18:09:01 +0530

    Updated LCM function

--- math/least-common-multiple/least_common_multiple.c
+++ math/least-common-multiple/least_common_multiple.c
@@ -4,14 +4,14 @@ int main()
     int n1, n2, lcm;
     printf("Enter two positive integers: ");
     scanf("%d %d", &n1, &n2);
-    // maximum number between n1 and n2 is stored in minMultiple
+    // maximum number between n1 and n2 is stored in lcm
     lcm = (n1>n2) ? n1 : n2;
     // Always true
     while(1)
     {
-        if( minMultiple%n1==0 && minMultiple%n2==0 )
+        if( lcm%n1==0 && lcm%n2==0 )
         {
-            printf("The LCM of %d and %d is %d.", n1, n2,minMultiple);
+            printf("The LCM of %d and %d is %d.", n1, n2,lcm);
             break;
         }
         ++lcm;
