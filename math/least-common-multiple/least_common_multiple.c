d28364d3a9e4532b095c496c3adec01d6469cb04
GauravSingh9356 <55052983+GauravSingh9356@users.noreply.github.com>
2019-10-26 18:00:54 +0530

    Shorted LCM function and added comments

--- math/least-common-multiple/least_common_multiple.c
+++ math/least-common-multiple/least_common_multiple.c
@@ -1,42 +1,20 @@
 #include <stdio.h>
-
-// Returns -1 on error, > 0 on success
-int get_least_common_multiple(int a, int b) {
-    int maximum = 0;
-
-    if (a <= 0 || b <= 0) {
-        return -1;
-    }
-    
-    maximum = (a > b)? a : b;
-    
-    while (1) {
-        if ((maximum % a == 0) && (maximum % b == 0)) {
-    		break;
-		}
-		maximum++;
-	}
-
-    return maximum;
-}
-
 int main()
 {
-    int a = 0;
-    int b = 0;
-    int least_common_multiple = 0;
-
-    while (1) {
-        printf("give two integers: \n");
-        scanf("%d %d", &a, &b);
-
-        if (-1 == (least_common_multiple = get_least_common_multiple(a, b))) {
-            fprintf(stderr, "Failed to get least common multiple for %d and %d!\n", a, b);
-            continue;
+    int n1, n2, lcm;
+    printf("Enter two positive integers: ");
+    scanf("%d %d", &n1, &n2);
+    // maximum number between n1 and n2 is stored in minMultiple
+    lcm = (n1>n2) ? n1 : n2;
+    // Always true
+    while(1)
+    {
+        if( minMultiple%n1==0 && minMultiple%n2==0 )
+        {
+            printf("The LCM of %d and %d is %d.", n1, n2,minMultiple);
+            break;
         }
-
-        printf(" least common multiple of %d and %d is %d\n", a, b, least_common_multiple);
+        ++lcm;
     }
-
     return 0;
 }
