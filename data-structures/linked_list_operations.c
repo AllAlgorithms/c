3f2aac536d8884bf8e8a958b82a4da6ec766de12
Shubham Samrat <shubham.samrat.min19@iitbhu.ac.in>
2020-04-10 00:32:53 +0530

    Better_linked_list_operations.c
    I Found this linked list better than the previous Written one.

--- data-structures/linked_list_operations.c
+++ data-structures/linked_list_operations.c
@@ -1,153 +1,614 @@
+//singly linked list C program and concept
+
 #include <stdio.h>
+
 #include <stdlib.h>
-struct llnode {
-    int data;
-    struct llnode *next;
-};
 
-void print(struct llnode *head) {
-    struct llnode *tmp;
-    tmp = head;
-    while (tmp != NULL) {
-        printf(" %d ", tmp->data);
-        tmp = tmp->next;
-    }
-}
-struct llnode *create(struct llnode *head) {
-    struct llnode *newnode, *tmp;
-    if (head == NULL) {
-        int n, i = 2;
-        printf("\nENTER NUMBER OF NODES=");
-        scanf("%d", &n);
-        head = (struct llnode *)malloc(sizeof(struct llnode));
-        printf("\nENTER ELEMENTS: ");
-        scanf("%d", &head->data);
-        newnode = head;
-        newnode->next = NULL;
-        while (i <= n) {
-            newnode->next = (struct llnode *)malloc(sizeof(struct llnode));
-            newnode = newnode->next;
-            scanf("%d", &newnode->data);
-            i++;
-        }
-        newnode->next = NULL;
-    }
-    printf("\n");
-    print(head);
-    return head;
-}
+#include <conio.h>
 
-void copy() {
-    struct llnode *head, *head1, *tmp, *newnode;
-    head = NULL;
-    head = create(head);
-    tmp = head;
-    head1 = (struct llnode *)malloc(sizeof(struct llnode));
-    newnode = head1;
-    newnode->data = tmp->data;
-    while (tmp->next != NULL) {
-        tmp = tmp->next;
-        newnode->next = (struct llnode *)malloc(sizeof(struct llnode));
-        newnode = newnode->next;
-        newnode->data = tmp->data;
-    }
-    newnode->next = NULL;
-    printf("\nDUPLICATE LinkedList Is\n");
-    print(head1);
-}
+struct node
 
-void concat() {
-    struct llnode *head1, *head2, *tmp;
-    head1 = NULL; head2 = NULL;
-    head1 = create(head1); head2 = create(head2);
-    tmp = head1;
-    while (tmp->next != NULL)
-        tmp = tmp->next;
-    tmp->next = head2;
-    head2 = NULL;
-    printf("\nCONCAT\n");
-    print(head1);
-}
+{
 
-void split() {
-    struct llnode *head1, *head2, *tmp;
-    int i = 1, n;
-    head1 = NULL;
-    head1 = create(head1);
-    printf("\nENTER POSITION TO SPLIT=");
-    scanf("%d", &n);
-    tmp = head1;
-    while (i < n - 1) {
-        tmp = tmp->next;
-        i++;
-    }
-    head2 = tmp->next;
-    tmp->next = NULL;
-    printf("\nll 1.\n");
-    print(head1);
-    printf("\nll 2.\n");
-    print(head2);
-}
+    int info;
 
-struct llnode* reverseR(struct llnode *head) {
-    if (head==NULL) {
-        return NULL;
-    } else if (head->next==NULL) {
-        return head;
-    } else {
-        struct llnode *rest=reverseR(head->next);
-        head->next->next=head;
-        head->next=NULL;
-        return rest;
-    }
-}
+    struct node *link;
 
-void reverse() {
-    struct llnode *head, *rev;
-    head = NULL;
-    head = create(head);
-    rev=reverseR(head);    
-    printf("\nREVERSE\n");
-    print(rev);
-}
+} * start;
 
-void noOfNodes() {
-    struct llnode *head, *tmp;
-    int n = 0;
-    head = NULL;
-    head = create(head);
-    tmp = head;
-    n++;
-    while (tmp->next != NULL) {
-        n++;
-        tmp = tmp->next;
-    }
-    printf("\nNUMBER OF NODES=%d", n);
-}
+void create(int);
 
-void main() {
-    int c;
-    while (1) {
-        printf("\nSELECT AN OPTION\n1.COPY\t2.CONCATENATE\t3.SPLIT\t4.REVERSE\t5.NO OF NODES\t6.Exit\n");
-        scanf("%d", &c);
-        switch (c) {
+struct node *create_node(int);
+
+void display(struct node *);
+
+void insert_end(int);
+
+void insert_beg(int);
+
+void insert_pos(int, int);
+
+void delete_beg();
+
+void delete_pos(int);
+
+void delete_end();
+
+void search(int);
+
+void reverse();
+
+int main()
+
+{
+
+    int choice, data, choice1, pos, no_of_node;
+
+    start = NULL;
+
+    while (1)
+
+    {
+
+        system("cls"); // equivalent to system("cls"); // equivalent to clrscr();
+
+        printf("1.Create Linked list\n");
+
+        printf("2.Display Linked list\n");
+
+        printf("3.Insert into Linked list\n");
+
+        printf("4.Delete Node in Linked list\n");
+
+        printf("5.Search in Linked list\n");
+
+        printf("6.Reverse the Linked list\n");
+
+        printf("7.Exit");
+
+        printf("\nEnter Your choice : ");
+
+        scanf("%d", &choice);
+
+        switch (choice)
+
+        {
+
         case 1:
-            copy();
+            printf("Enter no of node to be created :");
+
+            scanf("%d", &no_of_node);
+
+            create(no_of_node);
+
             break;
+
         case 2:
-            concat();
+            display(start);
+
             break;
+
         case 3:
-            split();
+            system("cls"); // equivalent to system("cls"); // equivalent to clrscr();
+
+            printf("1.Insert node at begining of list\n");
+
+            printf("2.Insert node at specific position in list\n");
+
+            printf("3.Insert node at End of list\n");
+
+            printf("4.previous menu\n");
+
+            printf("Enter Choice : ");
+
+            scanf("%d", &choice1);
+
+            switch (choice1)
+
+            {
+
+            case 1:
+                printf("Enter data for node :");
+
+                scanf("%d", &data);
+
+                insert_beg(data);
+
+                break;
+
+            case 2:
+                printf("Enter data for node :");
+
+                scanf("%d", &data);
+
+                printf("\n4Enter postition for node:");
+
+                scanf("%d", &pos);
+
+                insert_pos(data, pos);
+
+                break;
+
+            case 3:
+                printf("Enter data for node :");
+
+                scanf("%d", &data);
+
+                insert_end(data);
+
+                break;
+
+            case 4:
+                break;
+            }
             break;
+
         case 4:
-            reverse();
+            system("cls"); // equivalent to system("cls"); // equivalent to clrscr();
+
+            printf("1.Delete node at begining of list\n");
+
+            printf("2.Delete node at specific position in list\n");
+
+            printf("3.Delete node at End of list\n");
+
+            printf("4.previous menu\n");
+
+            printf("Enter Choice : ");
+
+            scanf("%d", &choice1);
+
+            switch (choice1)
+
+            {
+
+            case 1:
+                delete_beg();
+
+                break;
+
+            case 2:
+                printf("Enter postition for node:");
+
+                scanf("%d", &pos);
+
+                delete_pos(pos);
+
+                break;
+
+            case 3:
+                delete_end(data);
+
+                break;
+
+            case 4:
+                break;
+            }
             break;
+
         case 5:
-            noOfNodes();
+            system("cls"); // equivalent to system("cls"); // equivalent to clrscr();
+
+            printf("Enter Data to be searched :");
+
+            scanf("%d", &data);
+
+            search(data);
+
+            getch();
+
             break;
+
         case 6:
-            exit(0);
+            system("cls"); // equivalent to system("cls"); // equivalent to clrscr();
+
+            reverse();
+
+            break;
+
+        case 7:
+            exit(1);
+
+        default:
+            printf("Invalid Entry");
         }
     }
+
+    return 0;
+}
+
+void create(int no)
+
+{
+
+    int i, data;
+
+    system("cls"); // equivalent to system("cls"); // equivalent to clrscr();
+
+    struct node *temp;
+
+    temp = (struct node *)malloc(sizeof(struct node));
+
+    for (i = 0; i < no; i++)
+
+    {
+
+        if (start == NULL)
+
+        {
+
+            printf("Enter Data for node %d :", i);
+
+            scanf("%d", &data);
+
+            temp->info = data;
+
+            temp->link = NULL;
+
+            start = temp;
+        }
+
+        else
+
+        {
+
+            printf("Enter Data for node %d :", i);
+
+            scanf("%d", &data);
+
+            insert_end(data);
+        }
+    }
+
+    if (no > 0)
+
+        printf("List created");
+
+    else
+
+        printf("List not created");
+
+    getch();
+
+    system("cls"); // equivalent to system("cls"); // equivalent to clrscr();
+}
+
+void display(struct node *start)
+
+{
+
+    struct node *t;
+
+    t = start;
+
+    system("cls"); // equivalent to clrscr();
+
+    if (start == NULL)
+
+    {
+
+        printf("List is empty");
+
+        getch();
+
+        return;
+    }
+
+    printf("Linked List \n");
+
+    while (t != NULL)
+
+    {
+
+        printf("%d->", t->info);
+
+        t = t->link;
+    }
+
+    printf("End_of_list");
+
+    getch();
+
+    system("cls"); // equivalent to clrscr();
+}
+
+struct node *create_node(int data)
+
+{
+
+    struct node *temp;
+
+    temp = (struct node *)malloc(sizeof(struct node));
+
+    temp->info = data;
+
+    temp->link = NULL;
+
+    return temp;
+}
+
+void insert_end(int data)
+
+{
+
+    struct node *temp;
+
+    temp = start;
+
+    while (1)
+
+    {
+
+        if (temp->link != NULL)
+
+            temp = temp->link;
+
+        else
+
+            break;
+    }
+
+    temp->link = create_node(data);
+}
+
+void insert_beg(int data)
+
+{
+
+    struct node *newnode;
+
+    newnode = create_node(data);
+
+    newnode->link = start;
+
+    start = newnode;
+}
+
+void insert_pos(int data, int pos)
+
+{
+
+    int i;
+
+    struct node *newnode, *ptr;
+
+    ptr = start;
+
+    for (i = 0; i < pos; i++)
+
+    {
+
+        if (ptr == NULL)
+
+        {
+
+            printf("Invalid Position Entered");
+
+            getch();
+
+            return;
+        }
+
+        ptr = ptr->link;
+    }
+
+    if (ptr->link == NULL)
+
+    {
+
+        insert_end(data);
+    }
+
+    else
+
+    {
+
+        newnode = create_node(data);
+
+        newnode->link = ptr->link;
+
+        ptr->link = newnode;
+    }
+}
+
+void search(int data)
+
+{
+
+    int count = 0;
+
+    struct node *ptr;
+
+    ptr = start;
+
+    do
+    {
+
+        if (ptr->info == data)
+
+        {
+
+            printf("Data Found at position : %d", count);
+
+            printf("\nAddress of node with Data : %u", ptr);
+
+            return;
+        }
+
+        else
+
+            ptr = ptr->link;
+
+        count++;
+
+    } while (ptr->link != NULL);
+
+    printf("Data Not found");
+}
+
+void delete_beg()
+
+{
+
+    struct node *ptr;
+
+    if (start == NULL)
+
+    {
+
+        printf("List is empty");
+
+        return;
+    }
+
+    ptr = start;
+
+    start = ptr->link;
+
+    free(ptr);
+
+    printf("First Node Deleted");
+
+    getch();
+}
+
+void delete_pos(int pos)
+
+{
+
+    struct node *ptr, *preptr;
+
+    int i;
+
+    ptr = start->link;
+
+    preptr = start;
+
+    if (ptr == NULL)
+
+    {
+
+        delete_beg();
+
+        return;
+    }
+
+    for (i = 0; i < pos - 1; i++)
+
+    {
+
+        if (ptr == NULL)
+
+        {
+
+            printf("Invalid position");
+
+            getch();
+
+            return;
+        }
+
+        preptr = preptr->link;
+
+        ptr = ptr->link;
+    }
+
+    preptr->link = ptr->link;
+
+    free(ptr);
+}
+
+void delete_end()
+
+{
+
+    struct node *ptr, *preptr;
+
+    if (start == NULL)
+
+    {
+
+        printf("List is empty");
+
+        return;
+    }
+
+    ptr = start->link;
+
+    preptr = start;
+
+    while (ptr->link != NULL)
+
+    {
+
+        ptr = ptr->link;
+
+        preptr = preptr->link;
+    }
+
+    preptr->link = NULL;
+
+    free(ptr);
+
+    printf("Last Node Deleted");
+
+    getch();
+}
+
+void reverse()
+
+{
+
+    if (start == NULL)
+
+    {
+
+        printf("List is empty");
+
+        getch();
+
+        return;
+    }
+
+    else if (start->link == NULL)
+
+    {
+
+        printf("List reversed");
+
+        getch();
+
+        return;
+    }
+
+    struct node *p1, *p2, *p3;
+
+    p1 = start;
+
+    p2 = p1->link;
+
+    p3 = p2->link;
+
+    p1->link = NULL;
+
+    p2->link = p1;
+
+    while (p3 != NULL)
+
+    {
+
+        p1 = p2;
+
+        p2 = p3;
+
+        p3 = p3->link;
+
+        p2->link = p1;
+    }
+
+    start = p2;
+
+    printf("List Reversed");
+
+    getch();
 }
