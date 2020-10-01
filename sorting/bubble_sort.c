334ec66f4a3f284c2bd6829bdaaef3d40e1d7d83
tyadav4268 <60545022+tyadav4268@users.noreply.github.com>
2020-10-01 08:40:12 +0530

    Update bubble_sort.c
    reduced number of iterations on finding that there is no swap required that means the array is already sorted.

--- sorting/bubble_sort.c
+++ sorting/bubble_sort.c
@@ -17,11 +17,21 @@ void swap(int *x, int *y)
 void bubbleSort(int arr[], int n)
 {
 	int i, j;
+    int swapping = 0;
 	for (i = 0; i < n-1; i++)
+    {
 	// last i elements are already in place
 		for (j = 0; j < n-i-1; j++)
+        {
 			if (arr[j] > arr[j+1])
+            {
 				swap(&arr[j], &arr[j+1]);
+                swapping  = 1;
+            }
+        }
+        if(swapping = 0)    //if swapping is 0 that means the array is sorted
+            break;          // so no need to go for the next 
+    }
 }
 
 // Function to print elements
