230d17ac00ec6e7ee141e44d216f71ca86412b43
Hoodooloo <bhaskar.shashank3333@gmail.com>
2020-10-02 21:37:31 +0530

    Added Area of Triangle using both 3 sides and height method

---
+++ algorithms/math/Triangle/area.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <math.h>
+
+double areabyside(double, double, double);
+
+double areabyheight(double, double);
+
+int main()
+{
+    int n;
+    printf("Enter   1.For Sides \n\t2. For Height and base\n"); 
+    scanf("%d",&n);
+
+    switch(n)
+    {
+    case 1:
+        {
+            printf("Enter the sides of triangle: ");
+            double a, b, c;
+            scanf("%lf%lf%lf", &a, &b, &c);
+            double area = areabyside(a,b,c);
+            printf("Area of the given Triangle is %.2lf", area);
+            break;
+        }
+    case 2:
+        {
+            printf("Enter the height and base of triangle: ");
+            double h, b;
+            scanf("%lf%lf", &h, &b);
+            double area = areabyheight(h ,b);
+            printf("Area of the given Triangle is %.2lf", area);
+            break;
+        }
+    }
+}
+
+double areabyside(double a, double b, double c)
+{
+    double s, area;
+    s = (a+b+c)/2;
+    area = sqrt(s*(s-a)*(s-b)*(s-c));
+    return area;
+}
+double areabyheight(double h, double b)
+{
+    double area;
+    area = (h*b)/2;
+    return area;
+}
