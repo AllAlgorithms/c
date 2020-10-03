907b1041ea2a69e486b13953d82401fb8490acfd
Abraham Hernandez <abraham@abranhe.com>
2020-10-03 00:51:03 -0400

    Update selection_sort.c

--- algoritms/sorting/selection_sort.c
+++ algoritms/sorting/selection_sort.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-void SelectionSort(int A[], int size) //function definition
+void SelectionSort(int A[], int size)
 {
     int i,j;
-	for(i=0; i<size-1; i++)             //Loop for checking the lowest number in the array
+	for(i=0; i<size-1; i++)
 	{
 		int Imin = i;
-		for(j=i+1; j<size; j++)         //suppose i<j & when jth element in the array is less than ith element then jth element will become lowest number in the array
+		for(j=i+1; j<size; j++)
 		{
 			if( A[j] < A[Imin] )
 			{
 				Imin = j;
 			}
 		}
-		int temp = A[Imin];             //swap the Imin i.e jth element and ith element
+		int temp = A[Imin];
 		A[Imin] = A[i];
 		A[i] = temp;
 	}
@@ -26,9 +26,9 @@ int main()
     printf("Enter an array of size 10:\n");
     for(i=0; i<10; i++)
         scanf("%d", &num[i]);
-    SelectionSort(num, 10);         //Function call - Selection sort
+    SelectionSort(num, 10);
     printf("\nAfter Sorting:\n");
-    for(i=0; i<10; i++)             //printing the numbers in the sorted manner 
+    for(i=0; i<10; i++)
         printf("%d\t",num[i]);
     printf("\n");
     return 0;
