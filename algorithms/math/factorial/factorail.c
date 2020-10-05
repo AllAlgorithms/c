ef9c50dddb0dcf8919f550483ff6daf143311ff5
VIPUL662 <72247252+VIPUL662@users.noreply.github.com>
2020-10-05 16:47:57 +0530

    Update factorail.c
    Hello sir in this code there are three errors and I fixed them.

--- algorithms/math/factorial/factorail.c
+++ algorithms/math/factorial/factorail.c
@@ -1,24 +1,27 @@
 #include<stdio.h>
 void factorial(int n)
 {
-    if(n <= 0)
+    if(n <0)
     {
-        printf("\n Does not have factorial");
+        printf("\n factorial does not exist");
     }
     else
     {
         int f=1;
         while(n)
         {
-            f+=f*n;
+            f=f*n;
             n--;
         }
-        printf("\n Factorial of the number:%d",f);
+        printf("\n Factorial of the number is:%d",f);
     }
+     
 }
 int main()
 {
     int n;
     printf("\n Enter the value of n:");
-    scanf("%d",&f)
+    scanf("%d",&n);
+    factorial(n);
+    return 0;
 }
