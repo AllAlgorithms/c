4034c9872ac654c70af849733ff19a3f6a8d065d
AnanyaShreya <71565988+AnanyaShreya@users.noreply.github.com>
2020-10-05 14:29:12 +0530

    Added another method

--- algorithms/converting/decimal_to_binary.c
+++ algorithms/converting/decimal_to_binary.c
@@ -52,3 +52,38 @@ int main()
   printf("\n");
   return 0;
 }
+
+
+
+// I have another way of doing it using math.h library
+#include <math.h>
+#include <stdio.h>
+long long convert(int n);
+int main() {
+    int n;
+    printf("Enter a decimal number: ");
+    scanf("%d", &n);
+    printf("%d in decimal = %lld in binary", n, convert(n));
+    return 0;
+}
+
+long long convert(int n) {
+    long long bin = 0;
+    int rem, i = 1, step = 1;
+    while (n != 0) {
+        rem = n % 2;
+        printf("Step %d: %d/2, Remainder = %d, Quotient = %d\n", step++, n, rem, n / 2);
+        n /= 2;
+        bin += rem * i;
+        i *= 10;
+    }
+    return bin;
+}
+
+// here will explain the output
+// 21 / 2 = 10 with 1 remainder
+// 10 / 2 = 5 with 0 remainder
+// 5 / 2 = 2 with 1 remainder
+// 2 / 2 = 1 with 0 remainder
+// 1 / 2 = 0 with 1 remainder
+// so finally we get output:10101
