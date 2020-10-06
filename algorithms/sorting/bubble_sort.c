76dba9c2c3b0cbe2457d8f5d6ac4d3fa5ca518c5
scarlledo <64753842+scarlledo@users.noreply.github.com>
2020-10-06 21:35:10 +0530

    code cleanup

--- algorithms/sorting/bubble_sort.c
+++ algorithms/sorting/bubble_sort.c
@@ -1,56 +1,35 @@
-// C implementation of bubble sort
-// Author: Carlos Abraham Hernandez
-
-
 #include <stdio.h>
 
-// Swap elements
-void swap(int *x, int *y)
-{
-	int temp = *x;
-	*x = *y;
-  *y = temp;
-}
-
-// Implement bubble sort
-void bubbleSort(int arr[], int n)
-{
-	int i, j;
-    int swapping = 0;
-	for (i = 0; i < n-1; i++)
-    {
-	// last i elements are already in place
-		for (j = 0; j < n-i-1; j++)
-        {
-			if (arr[j] > arr[j+1])
-            {
-				swap(&arr[j], &arr[j+1]);
-                swapping  = 1;
-            }
-        }
-        if(swapping = 0)    //if swapping is 0 that means the array is sorted
-            break;          // so no need to go for the next 
-    }
-}
-
-// Function to print elements
-void printArray(int arr[], int size)
-{
-	int i;
-	for (i=0; i < size; i++)
-    printf("%d ", arr[i]);
-	printf("\n");
-}
-
-// Driver Code
 int main()
 {
-  int arr[] = {46, 24, 33, 10, 2, 81, 50};
-	int n = sizeof(arr)/sizeof(arr[0]);
-	printf("Unsorted array: \n");
-	printArray(arr, n);
-  bubbleSort(arr, n);
-  printf("Sorted array: \n");
-  printArray(arr, n);
+  int array[100], n, c, d, swap;
+
+  printf("Enter number of elements\n");
+  scanf("%d", &n);
+
+  printf("Enter %d integers\n", n);
+
+  for (c = 0; c < n; c++)
+    scanf("%d", &array[c]);
+
+  for (c = 0 ; c < n - 1; c++)
+  {
+    for (d = 0 ; d < n - c - 1; d++)
+    {
+      if (array[d] > array[d+1]) /* For decreasing order use < */
+      {
+        swap       = array[d];
+        array[d]   = array[d+1];
+        array[d+1] = swap;
+      }
+    }
+  }
+
+  printf("Sorted list in ascending order:\n");
+
+  for (c = 0; c < n; c++)
+     printf("%d\n", array[c]);
+
   return 0;
 }
+
