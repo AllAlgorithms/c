668fd0cdfe5fac062c32869e8ab527d5667268bc
Almajoo <72122115+Almajoo@users.noreply.github.com>
2020-10-06 15:41:02 +0530

    Updated program using structures

--- algorithms/math/sparse-matrix-illustrations/sparseTranspose.c
+++ algorithms/math/sparse-matrix-illustrations/sparseTranspose.c
@@ -1,136 +1,122 @@
-//
-// Sparse Matrix Transpose illustration
-//
-// The All ▲lgorithms Project
-//
-// https://allalgorithms.com/
-// https://github.com/allalgorithms/cpp
-//
-// Contributed by: Nikhil George Mathew
-// Github: @Nikcodes10
-//
-
-#include<cstdio>
-
-void transpose(int A[][3], int B[][3]) {
-
-	B[0][0] = A[0][1];
-	B[0][1] = A[0][0];
-	B[0][2] = A[0][2];
-	int z = 1;
-	for(int i = 1; i <= A[0][1]; i++) {
-		for(int j=1; j <= A[0][2]; j++) {
-
-			while(i == A[j][1] && j<=B[0][2]) {
-				B[z][0] = i;
-				B[z][1] = A[j][0];
-				B[z][2] = A[j][2];
-				z++;
-				j++;
-			}
-		}
-	}
-	printf("\n\tTranspose Sparse Matrix:\n");
-	for(int i=0; i<=B[0][2]; i++) {
-		printf("(%d, %d, %d)\n", B[i][0], B[i][1], B[i][2]);
-	}
-	printf("\n");
-}
-
-void matrixMaker(int tuple[][3]) {
-	printf("[\n");
-	for(int i=1; i<=tuple[0][0]; i++) {
-		for(int j=1; j<=tuple[0][1]; j++) {
-			int k = 1;
-			while(k <= tuple[0][2]) {
-				if(tuple[k][0] == i && tuple[k][1] == j) {
-					printf("%d ", tuple[k][2]);
-					k = -1;
-					break;
-				}
-				k++;
-			}
-			if(k!=-1) {
-				printf("%d ", 0);
-			}
-		}
-		printf("\n");
-	}
-	printf("]\n\n");
-}
-
-void tupleMaker(int r, int c, int matrix[][100], int nonzeros, int tuple[][3]) {
-	tuple[0][0] = r;
-	tuple[0][1] = c;
-	
-	int trow = 0;
-	for(int i=0; i<r; i++) {
-		for(int j=0; j<c; j++) {
-			if(matrix[i][j] != 0) {
-				trow++;
-				tuple[trow][0] = i+1;
-				tuple[trow][1] = j+1;
-				tuple[trow][2] = matrix[i][j];
-			}
-
-		}
-	}
-	tuple[0][2] = nonzeros;
-	printf("\n\tSparse Matrix:\n");
-	for(int i=0; i<=nonzeros; i++) {
-		printf("(%d, %d, %d)\n", tuple[i][0], tuple[i][1], tuple[i][2]);
-	}
-	printf("\n");
-}
-
-int r_c[2] = {-1, -1};
-
-int read(int matrix[][100]) {
-	int r, c;
-	if(r_c[0] == -1) {
-		printf("\nEnter no of rows: ");
-		scanf("%d", &r);
-		printf("Enter no of columns: ");
-		scanf("%d", &c);
-		r_c[0] = r;
-		r_c[1] = c;
-	}
-	else {
-		r = r_c[0];
-		c = r_c[1];
-	}
-	printf("\n\t\tMATRIX:\n");
-	int nonzeros = 0;
-
-	for(int i = 0; i < r; i++) {
-		for(int j = 0; j < c; j++)
-		{
-			printf("\nEnter %d,%d value: ", i+1, j+1);
-			scanf("%d", &matrix[i][j]);
-		}
-	}
-	printf("\t\tMATRIX:\n[\n");
-	for(int i=0; i<r; i++) {
-		for (int j = 0; j < c; j++)
-		{
-			printf("%d ", matrix[i][j]);
-			if(matrix[i][j] != 0) {
-				nonzeros++;
-			}
-		}
-		printf("\n");
-	}
-	printf("]\n");
-	return nonzeros+1;
-}
-
-void main() {
-	int matrixA[100][100];
-	int tupleA_length = read(matrixA);
-	int tupleA[100][3];
-	tupleMaker(r_c[0], r_c[1], matrixA, tupleA_length-1, tupleA);
-	int tupleTranspose[100][3];
-	transpose(tupleA, tupleTranspose);
-	printf("\tTranspose Matrix:\n");
-	matrixMaker(tupleTranspose);
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX_TERMS 101
+typedef struct {
+    int col;
+    int row;
+    int value;
+    }term; 
+ void transpose(term c[]); 
+ void create_sparse(int a[10][10],int row,int coloumn,term c[]);
+ void main()
+{
+    int a,b,c,d,i,j,ar1[10][10],ar2[10][10];
+    term x[MAX_TERMS], y[MAX_TERMS];
+    printf("\nEnter the row and coloumn size of the 1st matrix : ");
+    scanf("%d%d",&a,&b);
+    printf("Enter elements of 1st matrix row wise :\n");
+    for(i=0;i<a;i++)  //reading elements of first matrix
+    {
+        for(j=0;j<b;j++)
+        {
+          printf("Enter next element : ");
+          scanf("%d",&ar1[i][j]);
+        }
+    }
+    printf("\nEnter the row and coloumn size of the 2nd matrix : ");
+    scanf("%d%d",&c,&d);
+    printf("Enter elements of 2nd matrix row wise :\n");
+    for(i=0;i<c;i++)    //reading elements of second matrix
+    {
+        for(j=0;j<d;j++)
+        {
+          printf("Enter next element : ");
+          scanf("%d",&ar2[i][j]);
+        }
+    }  
+    printf("Entered matrixes are\n");   
+    for(i=0;i<a;i++)     //displaying 1st matrix
+    {
+        for(j=0;j<b;j++)
+        {
+          printf("%d  ",ar1[i][j]);
+        } 
+        printf("\n");
+    }  
+    printf("\n and \n");
+    for(i=0;i<c;i++)  //displaying 2nd matrix
+    {
+        for(j=0;j<d;j++)
+            printf("%d  ",ar2[i][j]);
+        printf("\n");
+    }  
+    printf("\nConverting the first matrix to triple form\n");
+    create_sparse(ar1,a,b,x);  
+    printf("\nConverting the second matrix to triple form\n");
+    create_sparse(ar2,c,d,y); 
+}  
+void create_sparse(int a[10][10],int row,int coloumn,term c[])
+{
+  int i,j,k=0,count;
+  c[0].row=row;
+  c[0].col=coloumn;
+  for(i=0;i<row;i++)   //checking for number of non-zero elements
+    {
+        for(j=0;j<coloumn;j++)
+        {
+          if(a[i][j]!=0)
+          k++;
+        } 
+    }
+   c[0].value=k;  
+   count=1;
+   for(i=0;i<row;i++)   //assigning value to triple matrix
+    {
+      for(j=0;j<coloumn;j++)
+        {
+          if(a[i][j]!=0)
+          {
+            c[count].row=i;
+            c[count].col=j;
+            c[count].value=a[i][j];
+            count++; 
+          } 
+        } 
+    }    
+   printf("\nSparse form list of matrix in triple form is\n");
+   for(i=0;i<=k;i++)  //printing triple form
+     printf("%d\t%d\t%d\n",c[i].row,c[i].col,c[i].value);
+   printf("\nTranspose of this matrix is\n");
+   transpose(c); 
+   
+} 
+void transpose(term c[])
+{
+    term d[MAX_TERMS];  //d is set to be the transpose of c
+    int i,j,n,count;
+    n=c[0].value;
+    d[0].row=c[0].col;
+    d[0].col=c[0].row;
+    d[0].value=n;
+    if(n>0)  //non-zero matrix
+    {
+        count=1;
+        for(i=0;i<c[0].col;i++)  //transpose by the coloumns in c
+        {
+            for(j=1;j<=n;j++)     //find elements from the current coloumn 
+            {
+              if(c[j].col==i)  
+               {        //elements in current coloumn added to d
+                  d[count].row=c[j].col;
+                  d[count].col=c[j].row;
+                  d[count].value=c[j].value;
+                  count++; 
+               } 
+            }
+        }
+    }
+    for(i=0;i<=d[0].value;i++)  //printing transverse of triple form
+    {
+     printf("%d\t%d\t%d\n",d[i].row,d[i].col,d[i].value);
+    }
 }
