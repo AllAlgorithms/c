c95c8ad9086ecf7071edcc37afcaacc57eb153d4
Ananya Shreya <71565988+AnanyaShreya@users.noreply.github.com>
2020-10-09 14:44:10 +0530

    Another method added
    I have added another method for conversion by using math.h

--- algorithms/converting/binary_to_decimal.c
+++ algorithms/converting/binary_to_decimal.c
@@ -21,3 +21,25 @@ int main() {
     printf("The number is: %d \n" ,dec);
     return 0;
 }
+// added another method
+#include <math.h>
+#include <stdio.h>
+int convert(long long n);
+int main() {
+    long long n;
+    printf("Enter a binary number: ");
+    scanf("%lld", &n);
+    printf("%lld in binary = %d in decimal", n, convert(n));
+    return 0;
+}
+
+int convert(long long n) {
+    int dec = 0, i = 0, rem;
+    while (n != 0) {
+        rem = n % 10;
+        n /= 10;
+        dec += rem * pow(2, i);
+        ++i;
+    }
+    return dec;
+}
