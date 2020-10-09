9c980cfbeeb469ab5765e7dff1e4789eaf3d65e4
Bhaskar srinivas Kurukuri <71520370+BhaskarSrinivasK@users.noreply.github.com>
2020-10-09 10:57:56 +0530

    better to understand

--- algorithms/swap_number_without_using_3rd_variable.c
+++ algorithms/swap_number_without_using_3rd_variable.c
@@ -1,19 +1,18 @@
-/*
-  E.g - x = 7 and y = 3
-  After swapping: x = 3 and y = 7
-*/
-
 #include <stdio.h> 
 int main() 
 { 
-	int x = 7, y = 3; 
+	int x,y;
+	
+        printf("Enter numbers to swap");
+	
+	scanf("%d %d",&x,&y); /*Takes input*/
+	
+	printf("Numbers before swapping are x = %d and y = %d",x,y); /* let x=3 and y=4*/
+	x = x + y; /*x=3+4=7*/
+	y = x - y; /*y=7-4=3*/
+	x = x - y; /*x=7-3=4*/
 
-	// Logic to swap Numbers 
-	x = x + y; // x becomes 10
-	y = x - y; // y becomes 7 
-	x = x - y; // x becomes 3 
-
-	printf("After Swapping: x = %d, y = %d", x, y); 
+	printf("Numbers after swapping are x = %d, y = %d", x, y); /*x=4 and y=3*/
 
 	return 0; 
 } 
