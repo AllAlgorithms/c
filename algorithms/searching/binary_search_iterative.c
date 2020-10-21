cbcc3ebab2077dee33950ffeee5ac8c7cffa59bb
TriptiPaliwal2000 <67820442+TriptiPaliwal2000@users.noreply.github.com>
2020-10-21 10:56:38 +0530

    Create binary_search_iterative.c

---
+++ algorithms/searching/binary_search_iterative.c
@@ -0,0 +1,41 @@
+//Binary Search is a searching algorithm for finding an element's position in a sorted array.
+
+//In this approach, the element is always searched in the middle of a portion of an array.
+
+//Iterative Binary Search in C
+
+// Binary Search in C
+
+#include <stdio.h>
+
+int binarySearch(int array[], int x, int low, int high) {
+  // Repeat until the pointers low and high meet each other
+  while (low <= high) {
+    int mid = low + (high - low) / 2;
+
+    if (array[mid] == x)
+      return mid;
+
+    if (array[mid] < x)
+      low = mid + 1;
+
+    else
+      high = mid - 1;
+  }
+
+  return -1;
+}
+
+int main(void) {
+  int array[] = {12, 20, 24, 56, 76, 81, 90};
+  int n = sizeof(array) / sizeof(array[0]);
+  int x = 4;
+  int result = binarySearch(array, x, 0, n - 1);
+  if (result == -1)
+    printf("Not found");
+  else
+    printf("Element is found at index %d", result);
+  return 0;
+}
+
+
