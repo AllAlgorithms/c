649a1e354c82249f172b987f3e7bd7538d4e0240
Prince <princerocknroll786@gmail.com>
2021-03-08 11:09:51 -0800

    Remove repeaditily instuction in quickSort

--- algorithms/quicksort.c
+++ algorithms/quicksort.c
@@ -1,31 +1,29 @@
 #include<stdio.h>
-void quicksort(int number[25],int first,int last){
-   int i, j, pivot, temp;
-
-   if(first<last){
-      pivot=first;
-      i=first;
-      j=last;
-
-      while(i<j){
-         while(number[i]<=number[pivot]&&i<last)
-            i++;
-         while(number[j]>number[pivot])
-            j--;
-         if(i<j){
-            temp=number[i];
-            number[i]=number[j];
-            number[j]=temp;
-         }
-      }
-
-      temp=number[pivot];
-      number[pivot]=number[j];
-      number[j]=temp;
-      quicksort(number,first,j-1);
-      quicksort(number,j+1,last);
-
-   }
+void quickSort(int array[],int first,int last)
+{
+    int i, j, pivot, temp;
+    if(first<last)
+    {
+        i=pivot=first;
+        j=last;
+        while(i<j)
+        {
+            while(array[i]<=array[pivot]&&i!=last)
+                i++;
+            while(array[j]>array[pivot]&&j!=first)
+                j--;
+            temp=array[j];
+            if(i<j)
+            {
+                array[j]=array[i];
+                array[i]=temp;
+            }
+        }
+        array[j]=array[pivot];
+        array[pivot]=temp;
+        quickSort(array,first,j-1);
+        quickSort(array,j+1,last);
+    }
 }
 
 int main(){
