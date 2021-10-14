3a6000f8c528cb0a4c84e20f40820e6e32a795d2
bankuri1 <65163703+bankuri1@users.noreply.github.com>
2021-10-14 18:30:21 +0530

    create bubble sort

---
+++ algorithms/bubble_sort.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main(){
+
+   int count, temp, i, j, number[30];
+
+   printf("How many numbers are u going to enter?: ");
+   scanf("%d",&count);
+
+   printf("Enter %d numbers: ",count);
+
+   for(i=0;i<count;i++)
+   scanf("%d",&number[i]);
+
+   /* This is the main logic of bubble sort algorithm 
+    */
+   for(i=count-2;i>=0;i--){
+      for(j=0;j<=i;j++){
+        if(number[j]>number[j+1]){
+           temp=number[j];
+           number[j]=number[j+1];
+           number[j+1]=temp;
+        }
+      }
+   }
+
+   printf("Sorted elements: ");
+   for(i=0;i<count;i++)
+      printf(" %d",number[i]);
+
+   return 0;
+}
