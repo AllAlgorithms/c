3984741ea44714379bf286e4e93c0a75151de44f
Shashi Raj <34605595+shashirajraja@users.noreply.github.com>
2021-10-17 12:55:50 +0530

    Update: Moved algorithm to a method and added docs

--- algorithms/searching/linear_search.c
+++ algorithms/searching/linear_search.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
  
+/**
+* @brief Method to search an element linearly
+* @params array the array of integers
+* @params size the size of the array
+* @params search the item to search in the array
+* @returns size if not found else the index of the element
+*/
+int linear_search(int *array, int size, int search) {
+  int c = 0;
+  for (c = 0; c < size; c++)
+   {
+     if (array[c] == search)    /* If required element is found */
+     {
+       return c;
+     }
+   }
+   return size;
+}
+
+/**
+* Main Function 
+*/
 int main()
 {
   int array[100], search, c, n;
@@ -14,17 +36,14 @@ int main()
  
   printf("Enter a number to search\n");
   scanf("%d", &search); //Enter the element to be searched
+  
+  //Calling the linear search method 
+  c = linear_search(array, n, search);
  
-  for (c = 0; c < n; c++)
-  {
-    if (array[c] == search)    /* If required element is found */
-    {
-      printf("%d is present at location %d.\n", search, c+1);
-      break;
-    }
-  }
   if (c == n)
     printf("%d isn't present in the array.\n", search);
+  else 
+    printf("%d is present at location %d.\n", search, c+1);
  
   return 0;
 }
